d44cf91ce34aca6cd6453a2cb9d2425a76d1a74b
Excute <oys1751@gmail.com>
2022-11-22 15:21:45 +0900

    fix scripts

--- server.js
+++ server.js
@@ -2,12 +2,41 @@ require("dotenv").config();
 
 const http = require("node:http");
 const fs = require("node:fs");
+const path = require("node:path");
+
+const mimeTypes = {
+	".html": "text/html",
+	".js": "text/javascript",
+	".css": "text/css",
+	".json": "application/json",
+	".png": "image/png",
+	".jpg": "image/jpg",
+	".gif": "image/gif",
+	".svg": "image/svg+xml",
+	".wav": "audio/wav",
+	".mp4": "video/mp4",
+	".woff": "application/font-woff",
+	".ttf": "application/font-ttf",
+	".eot": "application/vnd.ms-fontobject",
+	".otf": "application/font-otf",
+	".wasm": "application/wasm",
+	".ico": "image/x-icon",
+};
 
 const server = http.createServer((req, res) => {
-	// console.log(req.url);
-	fs.readFile(`./dist${req.url}`, (err, data) => {
-		res.writeHead(200, { "Content-Type": "text/html" });
-		res.write(data);
+	console.log(req.url);
+	let url = req.url;
+	if (req.url === "/") {
+		url = "/index.html";
+	}
+	fs.readFile(`./dist${url}`, (err, data) => {
+		if (err) {
+			res.writeHead(404, { "Content-Type": mimeTypes[".html"] });
+			console.error(err);
+		} else {
+			res.writeHead(200, { "Content-Type": mimeTypes[path.extname(url)] });
+			res.write(data);
+		}
 		res.end();
 	});
 });
