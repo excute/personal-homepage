4f772e3d15b0ea3389de1cb6eb55f3fd781cdac8
Excute <oys1751@gmail.com>
2022-11-22 14:47:49 +0900

    Init node things

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require("node:http");
+const fs = require("node:fs");
+
+const server = http.createServer((req, res) => {
+	console.log(req.url);
+	fs.readFile("./src/index.html", (err, data) => {
+		res.writeHead(200, { "Content-Type": "text/html" });
+		res.write(data);
+		res.end();
+	});
+});
+
+server.listen(8080);
