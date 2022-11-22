5f2f7a595c179229587000a09f467d11fb7d5c5e
Excute <oys1751@gmail.com>
2022-11-22 15:04:42 +0900

    fix script and run scripts

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const http = require("node:http");
 const fs = require("node:fs");
 
 const server = http.createServer((req, res) => {
-	console.log(req.url);
-	fs.readFile("./src/index.html", (err, data) => {
+	// console.log(req.url);
+	fs.readFile(`./dist${req.url}`, (err, data) => {
 		res.writeHead(200, { "Content-Type": "text/html" });
 		res.write(data);
 		res.end();
